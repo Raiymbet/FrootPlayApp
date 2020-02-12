d452c6d3b3f457663f70c710530cd9bb41cda5ad
r.tukpetov <tukpetov94.sdu@gmail.com>
2020-02-12 21:05:09 +0600

    Second commit

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,12 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+import { MatRippleModule } from '@angular/material';
+import { MatPaginatorModule } from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
+    MatRippleModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatCheckboxModule,
     HomeRoutingModule,
     CommonModule
-  ]
+  ],
+  exports: [
+    MatRippleModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatExpansionModule
+  ],
 })
 export class HomeModule { }
--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { movies } from 'src/app/shared/models/movie.mock-data';
+import { genreType } from 'src/app/shared/models/movie.model';
+import 'jarallax';
+import { PageEvent } from '@angular/material/paginator';
+declare var jarallax: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  movieBanners = [];
+  moviesList = [];
+  genres = [];
+  panelOpenState: boolean = true;
 
-  ngOnInit() {
+  // MatPaginator Inputs
+  length = movies.length;
+  pageSize = 10;
+  pageSizeOptions: number[] = [];
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+  
+  constructor() {
+    this.movieBanners.push(movies[Math.floor(Math.random() * movies.length)]);
+    this.movieBanners.push(movies[Math.floor(Math.random() * movies.length)]);
+    this.moviesList = movies.slice(0, this.pageSize);
   }
 
+  ngOnInit() {
+    this.genres = Object.values(genreType);
+  }
+
+  ngAfterViewInit() {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.1
+    }) 
+  }
+
+  goToMovieDetails(movie) {
+
+  }
 }
